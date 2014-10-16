ae593fd5de74a123b5064ef2e018b4955dc9e6c9
Matt Parrilla <matthew.parrilla@gmail.com>
2014-10-16 15:25:43 -0400

    Complete testing, change back to production code

--- runserver.py
+++ runserver.py
@@ -8,14 +8,13 @@ from upload_s3 import set_metadata
 from flask_frozen import Freezer
 
 # cron is called with 3 arguments, should only run in the first week of month
-cron_condition = len(sys.argv) == 3 and date.today().day > 8
+cron_condition = len(sys.argv) == 3 and date.today().day < 8
 force_update = len(sys.argv) == 2 and sys.argv[1] == 'freeze'
 
 if len(sys.argv) > 1:  # if runserver is passed an argument
     if cron_condition or force_update:
-        print "Cron succeeded"
-        #freezer = Freezer(app)
-        #freezer.freeze()
-        #set_metadata()
+        freezer = Freezer(app)
+        freezer.freeze()
+        set_metadata()
 else:
     app.run()
