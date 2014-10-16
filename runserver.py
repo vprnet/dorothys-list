b8af7c82dd2276ee6db02bf88f750dd69f2bc5b7
Matt Parrilla <matthew.parrilla@gmail.com>
2014-10-16 15:22:25 -0400

    Add print statement to update for testing

--- runserver.py
+++ runserver.py
@@ -2,13 +2,20 @@
 
 import sys
 
+from datetime import date
 from main import app
 from upload_s3 import set_metadata
 from flask_frozen import Freezer
 
-if len(sys.argv) > 1 and sys.argv[1] == 'freeze':
-    freezer = Freezer(app)
-    freezer.freeze()
-    set_metadata()
+# cron is called with 3 arguments, should only run in the first week of month
+cron_condition = len(sys.argv) == 3 and date.today().day < 8
+force_update = len(sys.argv) == 2 and sys.argv[1] == 'freeze'
+
+if len(sys.argv) > 1:  # if runserver is passed an argument
+    if cron_condition or force_update:
+        print "Cron succeeded"
+        #freezer = Freezer(app)
+        #freezer.freeze()
+        #set_metadata()
 else:
     app.run()
