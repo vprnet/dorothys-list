d09b5580ad7ef40f4d1a44e5b9903d4f8f70b2b2
Matt Parrilla <matthew.parrilla@gmail.com>
2014-10-16 15:24:50 -0400

    Tweak conditions for testing

--- runserver.py
+++ runserver.py
@@ -8,7 +8,7 @@ from upload_s3 import set_metadata
 from flask_frozen import Freezer
 
 # cron is called with 3 arguments, should only run in the first week of month
-cron_condition = len(sys.argv) == 3 and date.today().day < 8
+cron_condition = len(sys.argv) == 3 and date.today().day > 8
 force_update = len(sys.argv) == 2 and sys.argv[1] == 'freeze'
 
 if len(sys.argv) > 1:  # if runserver is passed an argument
