699deb0e10a2b874c0fe1ebd5284ce670b2ce173
SARA SIMON <smbsimon@gmail.com>
2015-11-04 09:37:51 -0500

    New Facebook image

--- main/views.py
+++ main/views.py
@@ -15,7 +15,7 @@ def index():
     social = {
         'title': "Dorothy's List",
         'subtitle': "VPR's Book Club For Kids",
-        'img': 'http://mediad.publicbroadcasting.net/p/vpr/files/201509/vpr-dorothy-main-image.jpg',
+        'img': 'http://mediad.publicbroadcasting.net/p/vpr/files/201510/dorothy-facebook-image.jpeg',
         'description': "Each month VPR highlights a book nominated for the Dorothy Canfield Fisher Children's Book Award.",
         'twitter_text': "Read along with Dorothy's List, a VPR book club for kids:",
         'twitter_hashtag': 'books,reading'
